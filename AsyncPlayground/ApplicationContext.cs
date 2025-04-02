962d0d27eb96954cf28af8327fcfe7ffced31bf6
KravetsB <bohdan.ok.ok.ok.ok.ok@gmail.com>
2025-04-02 14:56:01 +0300

    Fixed async/await issues

--- AsyncPlayground/ApplicationContext.cs
+++ AsyncPlayground/ApplicationContext.cs
@@ -1,16 +1,10 @@
 ﻿using AsyncPlayground.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AsyncPlayground
 {
     internal class ApplicationContext(DbContextOptions<ApplicationContext> options) : DbContext(options)
     {
-
         public DbSet<Employee> Employees { get; set; }
     }
 }
